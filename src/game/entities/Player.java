5f2b55aaf6336b430844a081c1858a51beb07d49
Jcaron429 <Jamie@Jamess-MacBook-Pro-3.local>
2014-03-13 04:05:52 -0400

    Updated Sprite Animations
    Added Sprite Animations & Added several new directional player sprites

--- src/game/entities/Player.java
+++ src/game/entities/Player.java
@@ -47,15 +47,26 @@ public class Player extends Mob {
 	public void render(Screen screen) {
 		int xTile = 0;
 		int yTile = 28;
+		int walkingSpeed = 3;
+		int flipTop = (numSteps >> walkingSpeed & 1);
+		int flipBottom = (numSteps >> walkingSpeed & 1);
+		
+		if(movingDir == 1){
+			xTile += 2;
+		} else if(movingDir > 1){
+			xTile += 4 + ((numSteps >> walkingSpeed) & 1) * 2;
+			flipTop = (movingDir -1) % 2;
+		}
 		
 		int modifier = 8 * scale;
 		int xOffset = x - modifier/2;
 		int yOffset = y - modifier/2 -4;
 		
-		screen.render(xOffset, yOffset, xTile + yTile * 32, color ,0x00, scale);
-		screen.render(xOffset + modifier, yOffset, (xTile + 1) + yTile * 32, color,0x00, scale);
-		screen.render(xOffset , yOffset + modifier, xTile + (yTile + 1) * 32, color,0x00, scale);
-		screen.render(xOffset + modifier, yOffset + modifier, (xTile + 1) + (yTile + 1) * 32, color,0x00, scale);
+		screen.render(xOffset + (modifier * flipTop), yOffset, xTile + yTile * 32, color ,flipTop, scale);
+		screen.render(xOffset + modifier - (modifier * flipTop), yOffset, (xTile + 1) + yTile * 32, color,flipTop, scale);
+		
+		screen.render(xOffset + (modifier * flipBottom) , yOffset + modifier, xTile + (yTile + 1) * 32, color,flipBottom, scale);
+		screen.render(xOffset + modifier - (modifier * flipBottom), yOffset + modifier, (xTile + 1) + (yTile + 1) * 32, color,flipBottom, scale);
 
 	}
 }
